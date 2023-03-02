7084fc015fb3b116bd6e2d261241658ea3c812a2
David Moreno <davidan7845@gmail.com>
2023-03-02 00:35:35 -0500

    se aplico una asignacion dinamica de botones por filtro de preguntas

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,25 +1,23 @@
 'use client';
 
 import Button from "./components/button.jsx"
-
-const handleClick = () => {
-  console.log('Se hizo clic en el botón');
-};
+import { INFORMATION } from "./utilities/interfaceInformation.js";
 
 Home.defaultProps = {
-  title: '¿Qué quieres hacer?',
+  informationPosition: 0,
 }
 
 export default function Home(props) {
   return (
     <>
       <section className="flex flex-col items-center ">
-          <h1 className="mt-20 mb-7 font-bold w-auto text-5xl">{props.title}</h1>
+          <h1 className="mt-20 mb-7 font-bold w-auto text-5xl">{INFORMATION[props.informationPosition].title}</h1>
           <div className="w-96 mb-12 h-px border border-t border-solid border-gray-line p-0 mx-auto"></div>
         <div className="flex flex-col gap-5 mb-5">
-          <Button texto="Sesión de estudio o trabajo" onClick={handleClick} />
-          <Button texto="Sesión de lectura" onClick={handleClick} />
-          <Button texto="Ver estadísticas" onClick={handleClick} />
+
+          {INFORMATION[props.informationPosition].buttons.map((button) => (
+            <Button texto={button.optionTitle} onClick={button.optionAction} />
+          ))}
         </div>
       </section>
     </>
