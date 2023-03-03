6f3855002e0ff829f1c2a061dc228cd485f76d3a
David-A-Moreno <davidan7845@gmail.com>
2023-03-03 15:10:42 -0500

    se definieron los sistemas de tiempos que se usaran dadas las respuestas del usuario

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -24,32 +24,45 @@ export default function Home() {
     return <div className="ml-8 w-32 h-12"></div>;
   }
 
+  /*
+    Define el tipo el sistema de tiempos de estudio y descanso que se va a utilizar
+    dependiendo de lo que se respondio en las preguntas iniciales:
+
+    1) 10 minutos de descanso en una hora donde el usuario elige cuando los toma, este tiempo descanso
+        no es acumulable con otras horas. Se puede tomar una pausa de emergencia que exceda los
+        10 minutos tomando los tiempos de descanso de las horas de estudio siguientes.
+    2) 15 minutos fijos de descanso cada 45 minutos, cada dos horas ese descanso aumenta 5 minutos
+    3) 5 minutos de descanso iniciales cada 25 minutos, este tiempo de descanso aumenta 5 minutos
+        cada ronda de estudio hasta un maximo de 20 minutos
+    4) 15 minutos fijos de descanso cada hora.
+  */
+
   const calculate = (key) => {
     if (currentInfo == 1) {
-      
+      return 2;
     }
     else if (currentInfo == 2) {
       if (key == 0) {
-        
+        return 2;
       }
       else {
-
+        return 3;
       }
     }
     else if (currentInfo == 3) {
       if (key == 0) {
-        
+        return 1;
       }
       else {
-
+        return 2;
       }
     }
     else {
       if (key == 0) {
-        
+        return 2;
       }
       else {
-
+        return 1;
       }
     }
   }
